74b002ea921c98bcbf42ef4172eb9680c75c9ba0
mrapan <kamphund@gmail.com>
2020-08-25 10:58:01 +0200

    Init commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace MassTransit7RabbitMQ
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                //sbc.Host("rabbitmq://localhost:5672");
+
+                sbc.Host("localhost", 5673, "notified-integration", hc =>
+                {
+                    hc.Username("notified_test");
+                    hc.Password("secret");
+                });
+
+                sbc.ReceiveEndpoint("test_queue", ep =>
+                {
+                    ep.Handler<TestCommand>(context => Console.Out.WriteLineAsync($"Received: {context.Message.Text}"));
+                });
+            });
+
+            await bus.StartAsync();
+
+            await bus.Publish(new TestCommand { Text = "Run!" });
+
+            Console.WriteLine("Press the any key");
+            await Task.Run(Console.ReadKey);
+
+            await bus.StopAsync();
+        }
+    }
+}
---
+++ TestCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace MassTransit7RabbitMQ
+{
+    public class TestCommand
+    {
+        public string Text { get; set; }
+    }
+}
